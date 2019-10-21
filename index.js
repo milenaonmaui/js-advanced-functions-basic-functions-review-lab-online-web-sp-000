03bfa185d71fd56c24bfbfb7cf2617f95ea7d2c2
Milena Kari <milenakari@yahoo.com>
2019-10-21 20:59:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ function saturdayFun (activity = 'roller-skate') {
 }
 
 function mondayWork (activity = 'got to the office')  {
-   return 'This Monday, I will '+ activity
+   return 'This Monday, I will '+ activity +'.'
+}
+
+function wrapAdjective(){
+  console.log('adj')
 }
