6462bdda79f05bed7ab6a6dc08a5d0d521653451
Milena Kari <milenakari@yahoo.com>
2019-10-21 21:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ var Calculator = {
  
  subtract : function (v1, v2){
    return v1-v2
- }
+ },
  
  multiply : function (v1, v2){
    return v1*v2
- }
+ },
  
  divide : function (v1, v2){
    return v1/v2
