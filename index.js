8945b9edb6ccdde748752e3f200797610c7f42c9
Milena Kari <milenakari@yahoo.com>
2019-10-21 21:00:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function saturdayFun (activity = 'roller-skate') {
   return 'This Saturday, I want to '+ activity+"!"
 }
 
-function mondayWork (activity = 'got to the office')  {
+function mondayWork (activity = 'go to the office')  {
    return 'This Monday, I will '+ activity +'.'
 }
 
